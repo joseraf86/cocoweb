c28ad1bc145aed21cb93348715bdf8f7085a4635
JR <joserafaelguevara@hotmail.com>
2020-05-08 13:07:34 +0200

    Poll views and controllers actions added

--- app/controllers/poll_controller.rb
+++ app/controllers/poll_controller.rb
@@ -1,7 +1,20 @@
 class PollController < ApplicationController
-  def new
+
+
+  # GET /poll/new
+  # View template already defined
+  # def new; end
+
+  # POST /poll/create
+  def create
+    flash[:notice] = 'Gracias por llenar la encuesta voluntaria. Ahora puede llevarse su bolsa CLAP, solamente si ingresó su nombre, correo electrónico y número de teléfono'
+    ## TODO: create a new Poll record
+    redirect_to poll_new_path
   end
 
-  def create
+  private
+  # Only allow a list of trusted parameters through.
+  def poll_params
+    params.permit(:name, :email, :phone, :select, :radio, :checkbox1, :checkbox2, :checkbox3, :open_question)
   end
 end
